03c7e08d07376b7938e7610ad087816af8c686e8
Steven <lopezstevie@gmail.com>
2020-06-18 09:25:48 -0400

    updated sample data

--- rpt-payment/src/assets/data/payment-data.ts
+++ rpt-payment/src/assets/data/payment-data.ts
@@ -7,8 +7,8 @@ export const PAYMENTDATA: PaymentHistory[] = [
   {accountNum: '83729', payee: '4', amount: 23.20, date: '3/20/2020', tags: 'sample text'},
   {accountNum: '83729', payee: '5', amount: 13, date: '4/20/2020', tags: 'sample text'},
   {accountNum: '83729', payee: '6', amount: 389, date: '6/20/2020', tags: 'sample text'},
-  {accountNum: '83729', payee: '7', amount: 3, date: '7/10/2020', tags: 'look'},
-  {accountNum: '83729', payee: '8', amount: 54, date: '8/15/2020', tags: 'sample text'},
-  {accountNum: '83729', payee: '9', amount: 7.89, date: '9/2/2020', tags: 'sample text'},
-  {accountNum: '83729', payee: '10', amount: 123, date: '5/10/2020', tags: 'sample text'},
+  {accountNum: '573820', payee: '7', amount: 3, date: '7/10/2020', tags: 'look'},
+  {accountNum: '573820', payee: '8', amount: 54, date: '8/15/2020', tags: 'sample text'},
+  {accountNum: '573820', payee: '9', amount: 7.89, date: '9/2/2020', tags: 'sample text'},
+  {accountNum: '93817', payee: '10', amount: 123, date: '5/10/2020', tags: 'sample text'},
 ];
