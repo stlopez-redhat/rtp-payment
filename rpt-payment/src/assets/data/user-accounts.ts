b4807dfec22a23799f892109fad1094f8148f960
Robb-Greathouse <rgreatho@redhat.com>
2020-10-06 14:59:19 -0600

    Update user-accounts.ts

--- rpt-payment/src/assets/data/user-accounts.ts
+++ rpt-payment/src/assets/data/user-accounts.ts
@@ -9,18 +9,19 @@ export const SYSTEMUSERS: UserProfile[] = [
     id: '2', title: 'Mr.', accountHolderID: '22222', firstName: 'Fred', lastName: '', address1: '', address2: '',
     city: '', state: '', zipCode: '', workPhone: '', cellPhone: '', email: '', role: 'user'
   },
+
   {
-    id: '3', title: '', accountHolderID: '33333', firstName: 'Red', lastName: 'Hat', address1: '100 East Davie Street', address2: '',
+    id: '3', title: 'Ms.', accountHolderID: '55555', firstName: 'Jane', lastName: 'Doe', address1: '', address2: '',
+    city: '', state: '', zipCode: '', workPhone: '', cellPhone: '', email: '', role: 'user'
+  },
+  {
+    id: '4', title: '', accountHolderID: '33333', firstName: 'Red', lastName: 'Hat', address1: '100 East Davie Street', address2: '',
     city: 'Raleigh', state: 'NC', zipCode: '27601', workPhone: '1-800-733-4281', cellPhone: '',
     email: 'customerservice@redhat.com', role: 'customerService'
   },
   {
-    id: '4', title: '', accountHolderID: '44444', firstName: 'Abraham', lastName: 'Lincoln', address1: '', address2: '',
+    id: '5', title: '', accountHolderID: '44444', firstName: 'Abraham', lastName: 'Lincoln', address1: '', address2: '',
     city: '', state: '', zipCode: '', workPhone: '', cellPhone: '', email: '', role: 'admin'
-  },
-  {
-    id: '5', title: 'Ms.', accountHolderID: '55555', firstName: 'Jane', lastName: 'Doe', address1: '', address2: '',
-    city: '', state: '', zipCode: '', workPhone: '', cellPhone: '', email: '', role: 'user'
   },
   {
     id: '6', title: '', accountHolderID: '66666', firstName: 'Dillards', lastName: 'Dillards', address1: '', address2: '',
